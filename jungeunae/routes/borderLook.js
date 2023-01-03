7a2a36593791f90de30c413622b9d6e6450dbfa6
JjungEeunAae <wjddmsdo77@naver.com>
2023-01-04 01:30:25 +0900

    0103 집

--- jungeunae/routes/borderLook.js
+++ jungeunae/routes/borderLook.js
@@ -1,10 +1,25 @@
+/** @format */
+
 //! 웹서버에서 동작하고자하는 컨트롤러를 실행
 var express = require("express");
 const pool = require("../mysql/pool");
 var router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("borderLook");
-});
+// borderLook_sql = {
+//     select: "select * from boarder_comment", //조회
+//     selectOne: "select * from boarder_comment where no = ?", //단건조회
+//     insert: "insert into boarder_comment set ?", //등록
+//     update: "update boarder_comment set ? where no = ?", //수정
+//     delete: "delete from boarder_comment where no = ?", //삭제
+// };
+
+// router.get("/", (req, res) => {
+//     pool.query(borderLook_sql.select, function (err, results, fields) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         res.json(results);
+//     });
+// });
 
 module.exports = router;
--- jungeunae/routes/users.js
+++ jungeunae/routes/users.js
@@ -1,31 +1,50 @@
+/** @format */
+
+//const cookieParser = require("cookie-parser");
 var express = require("express");
+const pool = require("../mysql/pool");
 var router = express.Router();
+
+loginsql = {
+    select: "select * from jungeunae", //조회
+    selectOne: "select * from jungeunae where userid = ?", //단건조회
+    insert: "insert into jungeunae set ?", //등록
+    view: "UPDATE jungeunae SET views = views + 1 WHERE no = ?", //조회수증가
+    update: "update jungeunae set ? where no = ?", //수정
+    delete: "delete from jungeunae where no = ?", //삭제
+};
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+    res.send("respond with a resource");
 });
 
 //!로그인 정보 가져오기
 router.get("/", (req, res) => {
-  sql = "SELECT * FROM jungeunae";
-  pool.query(sql, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    res.json(result);
-  });
+    pool.query(loginsql.select, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.json(result);
+    });
 });
 
 //!단건조회
 router.post("/login", (req, res) => {
-  const userid = req.body.userid;
-  const userpw = req.body.userpw;
-  sql = "SELECT * FROM jungeunae WHERE userid=?";
-  pool.query(sql, userid, (err, result) => {
-    //일치한 정보가 있으면 다음 페이지로 넘어가고
-    //아니면 경고창 띄우기
-  });
+    const userid = req.body.userid;
+    const userpw = req.body.userpw;
+    pool.query(loginsql.selectOne, userid, (err, result) => {
+        //일치한 정보가 있으면 다음 페이지로 넘어가고
+        if (result.length > 0) {
+            console.log("login success!!");
+            req.session.islogin = true;
+            res.send({ result: true });
+        } else {
+            //아니면 경고창 띄우기
+            console.log("loing failure!!");
+            res.send({ result: false });
+        }
+    });
 });
 
 module.exports = router;
