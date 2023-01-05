ed5c1456c22d5c6dce197d69acdd2b036667c828
Jung Eun Ae <wjddmsdo77@naver.com>
2023-01-05 17:54:45 +0900

    1205 학원

--- jungeunae/routes/border.js
+++ jungeunae/routes/border.js
@@ -8,136 +8,147 @@ var router = express.Router();
 // ! Prefix = /border
 
 sql = {
-    select: "select * from freeboard", //조회
-    selectOne: "select * from freeboard where no = ?", //단건조회
-    insert: "insert into freeboard set ?", //등록
-    view: "UPDATE freeboard SET views = views + 1 WHERE no = ?", //조회수증가
-    update: "update freeboard set ? where no = ?", //수정
-    delete: "delete from freeboard where no = ?", //삭제
-    comment: `
+  select: "select * from freeboard", //조회
+  insert: "insert into freeboard set ?", //등록
+  view: "UPDATE freeboard SET views = views + 1 WHERE no = ?", //조회수증가
+  update: "update freeboard set ? where no = ?", //수정
+  delete: "delete from freeboard where no = ?", //삭제
+  comment: `
       SELECT * 
       FROM boarder_comment`,
-    commentInsert: "INSERT into boarder_comment set ?",
-    commentDelete: "delete from boarder_comment where no = ?",
+  commentInsert: "INSERT into boarder_comment set ?",
+  commentDelete: "delete from boarder_comment where no = ?",
 };
-
-//!조회수
 
 //!전체조회
 router.get("/", (req, res) => {
-    pool.query(sql.select, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log(req.session);
-        res.render("border", {
-            islogin: req.session.islogin,
-            userid: req.session.userid,
-            list: result,
-        });
+  sql = `select no, userid, title, content, views, DATE_FORMAT(ydata,'%Y-%m-%d') ydata from freeboard;`;
+  pool.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(req.session);
+    res.render("border", {
+      islogin: req.session.islogin,
+      userid: req.session.userid,
+      list: result,
     });
+  });
 });
 
 // ! 글 작성 페이지 이동 (/border라는 url이 생략이 되어 있음)
 router.get("/writePage", (req, res) => {
-    pool.query(sql.select, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log(`Result => ${JSON.stringify(result)}`);
-        res.render("borderOne", {
-            islogin: req.session.islogin,
-            userid: req.session.userid,
-            list: result,
-        });
+  pool.query(sql.select, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Result => ${JSON.stringify(result)}`);
+    res.render("borderOne", {
+      islogin: req.session.islogin,
+      userid: req.session.userid,
+      list: result,
     });
+  });
 });
 
 // ! 글 작성 후 저장
 router.post("/writePage", (req, res) => {
-    pool.query(sql.insert, req.body, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.send(result);
-    });
+  pool.query(sql.insert, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
 });
 
 // ! 글 수정페이지 ( 이동 )
 router.get("/borderPut/:no", (req, res) => {
-    sql = "select * from freeboard where no = ?";
-    const no = req.params.no;
-    pool.query(sql, no, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.render("borderPut", { list: result });
-    });
+  sql = "select * from freeboard where no = ?";
+  const no = req.params.no;
+  pool.query(sql, no, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("borderPut", { list: result });
+  });
 });
 
 // ! 글 수정페이지 ( 게시글 수정 )
 router.put("/borderPut/:no", (req, res) => {
-    let data = [req.body, req.params.no];
-    pool.query(sql.update, data, (err, result) => {
-        let resultData = {};
-        if (result.changedRows > 0) {
-            resultData.result = true;
-            resultData.data = req.body;
-        } else {
-            resultData.result = false;
-        }
-        res.send(resultData);
-    });
+  let data = [req.body, req.params.no];
+  pool.query(sql.update, data, (err, result) => {
+    let resultData = {};
+    if (result.changedRows > 0) {
+      resultData.result = true;
+      resultData.data = req.body;
+    } else {
+      resultData.result = false;
+    }
+    res.send(resultData);
+  });
 });
 
 // ! 글 클릭하면 단건조회 페이지 이동
 router.get("/borderLook/:no", (req, res) => {
-    const no = req.params.no;
-    pool.query(sql.selectOne, no, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.render("borderLook", {
-            islogin: req.session.islogin,
-            userid: req.session.userid,
-            list: result,
-            bo_id: no,
-        });
+  var one =
+    "select userid, title, content, DATE_FORMAT(ydata,'%Y-%m-%d') ydata from freeboard where no = ?";
+  const no = req.params.no;
+  pool.query(one, no, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("borderLook", {
+      islogin: req.session.islogin,
+      userid: req.session.userid,
+      list: result,
+      bo_id: no,
     });
+  });
+});
+
+//!게시글 단건조회하면 조회수가 올라가는 곳
+router.post("/borderLook/:no", (req, res) => {
+  var sql = "update freeboard set views = ifnull(views,0)+1 where no = ?";
+  const no = req.params.no;
+  pool.query(sql, no, (err, result) => {
+    res.send(result);
+  });
 });
 
 // ! 댓글 전체조회
-router.get("/comment", (req, res) => {
-    // const bo_id = req.params.bo_id;
-    pool.query(sql.comment, (err, result) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        res.send(result);
-    });
+router.get("/comment/:bo_id", (req, res) => {
+  const bo_id = req.params.bo_id;
+  var sqlComment = `select no, bc_id, bc_content, DATE_FORMAT(bc_date,'%m월%d일') bc_date from boarder_comment where bo_id = ?`;
+  pool.query(sqlComment, bo_id, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 // ! 댓글 등록
 router.post("/comment", (req, res) => {
-    // const bo_id = req.params.bo_id;
-    pool.query(sql.commentInsert, req.body, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.json(result);
-    });
+  // const bo_id = req.params.bo_id;
+  pool.query(sql.commentInsert, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.json(result);
+  });
 });
 
 // ! 댓글 삭제
 router.delete("/comment/:no", (req, res) => {
-    const no = req.params.no;
-    pool.query(sql.commentDelete, no, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.send(result);
-    });
+  const no = req.params.no;
+  pool.query(sql.commentDelete, no, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
 });
 
 module.exports = router;
--- jungeunae/routes/notice.js
+++ jungeunae/routes/notice.js
@@ -6,62 +6,74 @@ const pool = require("../mysql/pool");
 var router = express.Router();
 
 notice_sql = {
-    select: "select * from noticeboder", //조회
-    selectOne: "select * from noticeboder where no = ?", //단건조회
-    insert: "insert into noticeboder set ?", //등록
-    update: "update noticeboder set ? where no = ?", //수정
-    delete: "delete from noticeboder where no = ?", //삭제
+  select: "select * from noticeboder", //조회
+  insert: "insert into noticeboder set ?", //등록
+  update: "update noticeboder set ? where no = ?", //수정
+  delete: "delete from noticeboder where no = ?", //삭제
 };
 
 //전체조회
 router.get("/", (req, res) => {
-    pool.query(notice_sql.select, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log(`Result => ${JSON.stringify(result)}`);
-        res.render("notice", {
-            islogin: req.session.islogin,
-            userid: req.session.userid,
-            list: result,
-        });
+  var sql = `select no, userid, title, content, views, DATE_FORMAT(ydata,'%Y-%m-%d') ydata from noticeboder`;
+  pool.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Result => ${JSON.stringify(result)}`);
+    res.render("notice", {
+      islogin: req.session.islogin,
+      userid: req.session.userid,
+      list: result,
     });
+  });
 });
 
 // ! 글 작성 페이지 이동 (/notice url이 생략이 되어 있음)
 router.get("/writePage", (req, res) => {
-    pool.query(notice_sql.select, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log(`Result => ${JSON.stringify(result)}`);
-        res.render("noticeOne", {
-            islogin: req.session.islogin,
-            userid: req.session.userid,
-            list: result,
-        });
+  pool.query(notice_sql.select, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Result => ${JSON.stringify(result)}`);
+    res.render("noticeOne", {
+      islogin: req.session.islogin,
+      userid: req.session.userid,
+      list: result,
     });
+  });
 });
 
 // ! 글 작성 후 저장
 router.post("/writePage", (req, res) => {
-    pool.query(notice_sql.insert, req.body, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.send(result);
-    });
+  pool.query(notice_sql.insert, req.body, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(result);
+  });
 });
 
 // ! 글 클릭하면 단건조회 페이지 이동
 router.get("/noticeLook/:no", (req, res) => {
-    const no = req.params.no;
-    pool.query(notice_sql.selectOne, no, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        res.render("noticeLook", { list: result });
-    });
+  var sql =
+    "select no, userid, title, content, DATE_FORMAT(ydata,'%Y-%m-%d') ydata from noticeboder where no = ?";
+  const no = req.params.no;
+  pool.query(sql, no, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("noticeLook", { list: result });
+  });
+});
+
+// ! 단건 게시글 클릭하면 조회수 증가
+router.post("/noticeLook/:no", (req, res) => {
+  var sql = "update noticeboder set views = ifnull(views,0)+1 where no = ?";
+  console.log(sql);
+  const no = req.params.no;
+  pool.query(sql, no, (err, result) => {
+    res.send(result);
+  });
 });
 
 module.exports = router;
